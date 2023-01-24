c99ec9a32edadb35e509fb9ae7f20fa17f985b18
Régis <loyaute.regis21@gmail.com>
2023-01-25 00:14:37 +0100

    k3s ansible first commit

---
+++ repo-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "creating inventory path and copying sample to inventory/my-cluster.."
+cp -R inventory/sample inventory/my-cluster
+echo "Done."
+echo "===================================================================="
+echo "Next step: Edit your inventory/my-cluster/hosts.ini"
+echo "Second: Edit inventory/my-cluster/group_vars/all.yml to your liking"
+echo "===================================================================="
